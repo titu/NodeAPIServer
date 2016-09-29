fab421963c7058ee0b19760667d3e7fc641dd5d0
titu <email.to.titu@gmail.com>
2016-09-30 02:20:08 +0600

    The logErrors and handleErrors method updated to make it work properly with bunyan logger

--- middleware/error.js
+++ middleware/error.js
@@ -1,16 +1,22 @@
 /**
  * Created by titu on 9/28/16.
  */
+const log = require('../helper').log;
 
 let logErrors = (err, req, res, next) => {
-    console.error(err.stack);
+    log.error(
+        'Error in handling request'
+        , { originalUrl: req.originalUrl }
+        , { originalMethod: req.originalMethod }
+        , { error: err }
+    );
     next(err);
 };
 
-let handleErrors = (err, req, res) => {
-    res.status(err.status || 500).json({
+let handleErrors = (err, req, res, next) => {
+    res.status(500).json({
         success: false,
-        message: err.status ? err.message : [
+        message: [
             'Error in handling this request. ',
             'Please contact system admin if you are continuously seeing this message.'
         ].join('')
